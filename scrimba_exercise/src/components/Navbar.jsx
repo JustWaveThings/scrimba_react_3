e6da0836b17ce528b74d03b345386d6835639330
JustWaveThings <justwavethings@proton.me>
2023-04-19 17:48:56 -0400

    Nav styled

--- scrimba_exercise/src/components/Navbar.jsx
+++ scrimba_exercise/src/components/Navbar.jsx
@@ -1,5 +1,16 @@
 function Navbar() {
-	return <h1> Navbar goes here </h1>;
+	return (
+		<div className="Navbar">
+			<img
+				src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png"
+				className="App-logo"
+				alt="logo"
+				height="28.93px"
+			/>
+			<h2>ReactFacts</h2>
+			<h3>React Course - Project 1</h3>
+		</div>
+	);
 }
 
 export default Navbar;
